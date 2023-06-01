1a2521532871b0ecd79a136cb162411214422693
hrdtbs <harada.tsubasa.0422@gmail.com>
2023-06-01 15:30:47 +0900

    fix: placeholder

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -42,7 +42,7 @@ export const Routes = (props: {
                     type="text"
                     required
                     defaultValue={route?.from}
-                    placeholder="池袋駅"
+                    placeholder="池袋"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   />
                 </div>
@@ -54,7 +54,7 @@ export const Routes = (props: {
                     name="to"
                     type="text"
                     required
-                    placeholder="東京駅"
+                    placeholder="神楽坂"
                     defaultValue={route?.to}
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   />
